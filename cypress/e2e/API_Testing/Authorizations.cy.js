969cf99dca49b3c081f156904188ea356dfd384d
Swapnaneel Ghosh <swapnaneel.ghosh@geotechinfo.net>
2023-08-22 15:11:39 +0530

    Authentication Modified

---
+++ cypress/e2e/API_Testing/Authorizations.cy.js
@@ -0,0 +1,31 @@
+describe('Authentication',()=>{
+    it('Basic Authentication',()=>{
+        cy.request({
+            method : 'GET',
+            url : 'https://postman-echo.com/basic-auth',
+            auth : {
+                user : 'postman',
+                pass : 'password'
+            }
+        })
+        .then((response)=>{
+            expect(response.status).to.eq(200)
+            expect(response.body.authenticated).to.eq(true)
+        })
+    })
+    it('Digest Authentication',()=>{
+        cy.request({
+            method : 'GET',
+            url : 'https://postman-echo.com/basic-auth',
+            auth : {
+                username : 'postman',
+                password : 'password',
+                //method : 'digest'
+            }
+        })
+        .then((response)=>{
+            expect(response.status).equals(200)
+            expect(response.body.authenticated).to.equals(true)
+        })
+    })
+})
