c305cedf6d46de72016945e9267b4a255e27ab22
Swapnaneel Ghosh <swapnaneel.ghosh@geotechinfo.net>
2023-08-22 16:11:51 +0530

    Auth Tests Pushed

--- cypress/e2e/API_Testing/Authorizations.cy.js
+++ cypress/e2e/API_Testing/Authorizations.cy.js
@@ -28,4 +28,56 @@ describe('Authentication',()=>{
             expect(response.body.authenticated).to.equals(true)
         })
     })
+
+    let authtoken = null
+    
+    before('Generating Bearer Access Token',()=>{
+        cy.request({
+            method : 'POST',
+            url : 'https://simple-books-api.glitch.me/api-clients/',
+            headers : {
+                'Content-type' : 'application/json'
+            },
+            body : {
+                clientName : 'ABC',
+                clientEmail : Math.random().toString(5).substring(2) + "@gmail.com"
+            }
+        }).then((response)=>{
+            authtoken = response.body.accessToken;
+
+        })
+    })
+
+
+    it('Creating a New Order with that Bearer Token',()=>{
+        cy.request({
+            method : 'POST',
+            url : 'https://simple-books-api.glitch.me/orders/',
+            headers : {
+                'Content-type' : 'application/json',
+                'Authorization' : 'Bearer '+authtoken
+            },
+            body : {
+                bookId : 1,
+                customerName : "Swapnaneel"
+            }
+        }).then((response)=>{
+            expect(response.status).eq(201)
+            expect(response.body.created).to.eq(true)            
+        })
+    })
+
+    it('API Key Authentication',()=>{
+        cy.request({
+            method : 'GET',
+            url : 'api.openweathermap.org/data/2.5/forecast/daily',
+            qs : {
+                q : 'Kolkata',
+                appid : 'fe9c5cddb7e01d747b4611c3fc9eaf2c'
+            }
+        })
+        .then((response)=>{
+            expect(response.status).to.eq(200)
+        })
+    })
 })
